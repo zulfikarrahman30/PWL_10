706ec25d8d59548768ae7bea6be8e0c2f28e454f
ZulfikarRahman <azuraromanov@gmail.com>
2021-04-13 22:49:51 +0700

    revisi repo sebelumnya

--- resources/views/articles/article_pdf.blade.php
+++ resources/views/articles/article_pdf.blade.php
@@ -1,37 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
+	<title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
 </head>
 <body>
-<style type="text/css">
-table tr td,
-table tr th{
-font-size: 9pt;
-}
+	<style type="text/css">
+	table tr td,table tr th{font-size: 9pt;}
 </style>
-<center>
-<h5>Laporan Artikel</h4>
-</center>
-<table class='table table-bordered'>
-<thead>
-<tr>
-<th>No</th>
-<th>Judul</th>
-<th>Isi</th>
-<th>Gambar</th>
-</tr>
-</thead>
-<tbody>
-@foreach($articles as $a)
-<tr>
-<td>{{ $loop->iteration }}</td>
-<td>{{$a->title}}</td>
-<td>{{$a->content}}</td>
-<td>{{$a->featured_image}}</td>
-</tr>
-@endforeach
-</tbody>
-</table>
-</body>
-</html>
+	<center>
+		<h5>Laporan Artikel</h4>
+		</center>
+		<table class='table table-bordered'>
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Judul</th>
+					<th>Isi</th>
+					<th>Gambar</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($articles as $a)
+				<tr>
+					<td>
+						{{ $loop->iteration}}
+					</td>
+					<td>{{$a->title}}</td>
+					<td>{{$a->content}}</td>
+					<td><img width="150px" src="{{asset('storage/'.$a->featured_image)}}"></td>
+				</tr>
+				@endforeach
+			</tbody>
+			</table>
+		</body>
+		</html>
